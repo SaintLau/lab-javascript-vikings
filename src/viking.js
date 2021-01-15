c201c05df296862a1c6a00d0ab199a8052c4ec4f
Laura Santos <alsr16p@gmail.com>
2021-01-15 15:14:04 +0000

    soldier done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,51 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+         return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+ }
+ 
 
-// Viking
-class Viking {}
+// Viking  
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+       super(name, health);
+        this.strength = strength;
+    } 
+}
+
+    
+
+
+/*
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+        
+    }
+    receiveDamage { //IS diferrent from Soldier but should extend Soldier
+    }
+    attack() {      //SHOULD be inherited from Soldier
+
+    }
+}
+
 
 // War
-class War {}
+class War {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    
+}
+*/
